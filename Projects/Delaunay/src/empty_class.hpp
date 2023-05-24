499e78b22430ad93a518d43da22db50b9f2ef353
Emilio <s281245@studenti.polito.it>
2023-05-24 12:53:54 +0200

    nonno

--- Projects/Delaunay/src/empty_class.hpp
+++ Projects/Delaunay/src/empty_class.hpp
@@ -1,12 +1,4 @@
-//Riga aggiunta da Maddi
-//Altra riga aggiunta da Maddi
-//Ennesima riga aggiunta da Maddi
-<<<<<<< HEAD
-//Ennesima riga aggiunta da Emilio
-=======
-//Enneunesima riga aggiunta da Maddi
->>>>>>> 08f9c93a1e995e17423cc27b458e61d9c62ed396
-
+// anubi
 #ifndef __EMPTY_H
 #define __EMPTY_H
 
@@ -18,24 +10,8 @@ namespace ProjectLibrary
   class Empty
   {
     public:
-      cout << "aldo" << endl;
       void Show() const { std::cout<< "Hello world;"<< std::endl; }
   };
 }
 
 #endif // __EMPTY_H
-<<<<<<< HEAD
-grrrrEmilio
-Evviva la vita
-
-//Commento aggiunto da Maddi
-=======
-// Lontani
-<<<<<<< HEAD
-//ollo
-ghjghjghjgj
-=======
-// ollo
-// provaemilio
-// anubi
->>>>>>> 7388a79fd76ef5dc90689bb89172bbe01a93da8f
