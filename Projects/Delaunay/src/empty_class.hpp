562d604a0998ce393e03475b83857e448e63fb99
Maddalena <s281604@studenti.polito.it>
2023-05-24 12:47:35 +0200

    Maddi 5

--- Projects/Delaunay/src/empty_class.hpp
+++ Projects/Delaunay/src/empty_class.hpp
@@ -1,12 +1,3 @@
-//Riga aggiunta da Maddi
-//Altra riga aggiunta da Maddi
-//Ennesima riga aggiunta da Maddi
-<<<<<<< HEAD
-//Ennesima riga aggiunta da Emilio
-=======
-//Enneunesima riga aggiunta da Maddi
->>>>>>> 08f9c93a1e995e17423cc27b458e61d9c62ed396
-
 #ifndef __EMPTY_H
 #define __EMPTY_H
 
@@ -24,18 +15,3 @@ namespace ProjectLibrary
 }
 
 #endif // __EMPTY_H
-<<<<<<< HEAD
-grrrrEmilio
-Evviva la vita
-
-//Commento aggiunto da Maddi
-=======
-// Lontani
-<<<<<<< HEAD
-//ollo
-ghjghjghjgj
-=======
-// ollo
-// provaemilio
-// anubi
->>>>>>> 7388a79fd76ef5dc90689bb89172bbe01a93da8f
